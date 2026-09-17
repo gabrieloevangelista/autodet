7468ff35c26a9f3e2042eeb9091ad635adfaf48d
gabrieloevangelista <gabriel.evan.queiroz@gmail.com>
2026-09-16 22:56:06 -0300

    feat: replace image inspection script with JPEG dimension extraction utility

--- test-img.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-// Simple JPEG decoder to inspect basic data or check file
-['scangrip-inspection.jpg', 'ceramic-application.jpg', 'interior-detailing.jpg', 'polishing-machine.jpg', 'snow-foam-luxury.jpg', 'brush-detailing.jpg'].forEach(name => {
-  const buf = fs.readFileSync('public/images/' + name);
-  console.log(name, 'size:', buf.length, 'header:', buf.slice(0, 4).toString('hex'));
-});
